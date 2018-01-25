daf119930b340024f615048fc8ec239152564d6a
hang.yang <hang.yang@92up.cn>
2018-01-26 00:14:01 +0800

    young

--- simpleToken/contracts/SmartDistribution.sol
+++ simpleToken/contracts/SmartDistribution.sol
@@ -1,48 +1,62 @@
 pragma solidity ^0.4.17;
 
-contract Token {
-    function transfer(address _to, uint _value) public returns (bool success);
-    function balanceOf(address _owner) constant public returns (uint balance);
-}
+import "Token.sol";
 
 contract SmartDistribution {
 
     mapping (address => uint256) public balance;
+    address[] addressList;
     uint256 public totalSupply;
     Token[] public supportTokens;
-    mapping(address => mapping (address => uint256)) public alreadyBalances;
     mapping(address => uint256) public tokenBalance;
 
-    mapping(address=>uint256) public sumTotalBalance;
-
-    function SmartDistribution(address[] _as,uint256[] _bs) {
+  function SmartDistribution(address[] _as,uint256[] _bs) {
         require(_as.length > 0);
         require(_as.length == _bs.length);
         for (uint i = 0; i <  _as.length; i++) {
             balance[_as[i]]= _bs[i];
             totalSupply+=_bs[i];
         }
+        addressList = _as;
     }
 
     function support(Token[] tokens) {
         for(uint i = 0 ; i < tokens.length ; i++){
-            supportTokens.push(tokens[i]);
+            Token t = tokens[i];
+            uint val =tokenBalance[t];
+            if(val>0){
+              continue;
+            }
+            supportTokens.push(t);
+            tokenBalance[t] =  t.balanceOf(this);
         }
-
     }
 
-    function claim(Token token)  {
-        require(balance[msg.sender]>0);
-        uint256 bal =token.balanceOf(this);
-        if(bal > tokenBalance[token]){
-            uint256 sub= bal - tokenBalance[token];
-            tokenBalance[token] = bal;
-            sumTotalBalance[token] += sub;
-        }
-
-        uint val = ((balance[msg.sender]/totalSupply)*sumTotalBalance[token])-tokenBalance[token];
-        token.transfer(msg.sender,val);
+    function claim(Token token){
+      this.claimTo(token, msg.sender);
     }
 
+    function claimTo(Token token,address ads) {
+
+          require(balance[ads]>0);
+          uint val = (balance[ads]/totalSupply)*tokenBalance[token];
+          token.transfer(ads,val);
+    }
+
+
+        function () payable {
+          for(uint i = 0;i< supportTokens.length;i++){
+            this.claim(supportTokens[i]);
+          }
+        }
+
+    function claimAll() {
+      for(uint i = 0;i< addressList.length;i++){
+        for(uint j = 0;j< supportTokens.length;i++){
+          this.claimTo(supportTokens[j],addressList[i]);
+        }
+      }
+
+    }
 
 }
